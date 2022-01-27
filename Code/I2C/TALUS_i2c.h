a3c55b93481fccd9551718d685751917c85dfbdf
is0p0d <jpmoroney715@outlook.com>
2022-01-27 15:51:36 -0600

    i2c work v0.6

--- Code/I2C/TALUS_i2c.h
+++ Code/I2C/TALUS_i2c.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>         //fixed with variables
 
-#define MAX_BUFF 64
+#define MAX_BUFF 4
 
 struct test_package_struct
 {
@@ -11,5 +11,19 @@ struct test_package_struct
     uint8_t param1;
     uint8_t param2;
 }__attribute__((packed));
+typedef test_package_struct dataPkg;
+
+void bufferUnpack(uint8_t* buffer, dataPkg* pkg)
+{
+    pkg->funcNum = buffer[0];
+    pkg->param1 = buffer[1];
+    pkg->param2 = buffer[2];
+}
+void bufferPack(uint8_t* buffer, dataPkg* pkg)
+{
+    buffer[0] = pkg->funcNum;
+    buffer[1] = pkg->param1;
+    buffer[2] = pkg->param2;
+}
 
 #endif //TALUS_I2C_H
--- Code/I2C/ard_i2c.cpp
+++ Code/I2C/ard_i2c.cpp
@@ -7,6 +7,8 @@
 #define ADDR 0x8 // i2c bus address must be something that can be changed
                  // many items do not have flexible i2c addresses
 
+dataPkg test;
+
 void setup()
 {
     Wire.begin(ADDR) // join the i2c bus using the defined address
@@ -16,10 +18,26 @@ void setup()
 void receivedEvent()
 {
     int position = 0;
-    char buffer[MAX_BUFF] = {0}
+    uint8_t buffer[MAX_BUFF] = {0}
     while(Wire.available()) //loop through all the values currently received
     {
         buffer[position] = Wire.read();
         position++;
     }
+    bufferUnpack(buffer, test); // found in TALUS_i2c.h
+    switch(test.funcNum) // what do I do now?
+    {
+        case 1:
+            //placeholder
+            break;
+        case 2:
+            break;
+        default:
+            //write error handler, i2c bidirectional comm
+            break;
+    }
+}
+void loop()
+{
+    delay(100);
 }
