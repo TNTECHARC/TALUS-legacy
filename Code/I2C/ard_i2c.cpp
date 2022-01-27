ff7a1a0d5f3a1a08a1bb33811acc81b2f6f4b7d9
is0p0d <jpmoroney715@outlook.com>
2022-01-27 14:15:29 -0600

    i2c work v0.1

---
+++ Code/I2C/ard_i2c.cpp
@@ -0,0 +1,12 @@
+//Talus I2C Communication for Arduino
+//Jim Moroney 2022
+
+#include <Wire.h> // the i2c library
+
+#define ADDR 0x8 // i2c bus address must be something that can be changed
+                 // many items do not have flexible i2c addresses
+
+void setup()
+{
+    Wire.begin(ADDR) // join the i2c bus using the defined address
+}
---
+++ Code/I2C/rpi_i2c.c
@@ -0,0 +1,5 @@
+#include <unistd.h>         //i2c library support
+#include <fcntl.h>          //i2c library support
+#include <sys/ioctl.h>      //i2c library support
+#include <linux/i2c-dev.h>  //i2c library support
+
