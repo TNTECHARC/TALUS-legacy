26939a85a30f05e0d92b27f600249cbaf525051f
is0p0d <jpmoroney715@outlook.com>
2022-01-27 14:42:29 -0600

    i2c work v0.3

--- Code/I2C/rpi_i2c.c
+++ Code/I2C/rpi_i2c.c
@@ -1,13 +1,23 @@
 //TALUS i2c communication server (Raspberry Pi)
+//Jim Moroney 2022
 
 #include <unistd.h>         //i2c library support
 #include <fcntl.h>          //i2c library support
 #include <sys/ioctl.h>      //i2c library support
 #include <linux/i2c-dev.h>  //i2c library support
-#include <stdint.h>         //fixed with variables
 #include <stdio.h>          //io capabilities
+#include <TALUS_i2c.h>      //various stuff for talus
 
-struct testPkg
+
+int main()
 {
-    uint
+    int i2c_bus
+
+    char* busPATH = (char*)"/dev/i2c-1";
+    if ((i2c_bus = open(busPATH, 0_RDRW)) < 0) // Open and varify the i2c bus.
+    {
+        printf("Failed to open the i2c bus");
+        return;
+    }
+    return 0;
 }
